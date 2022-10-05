9dd49020a32e5180bcd3002c43917b578fb79165
Lilian Fernandes Oliveira Dias <biologia.lilianfernandes@gmail.com>
2022-10-05 14:55:36 -0300

    Correção caminho das páginas

--- scripts/theme.js
+++ scripts/theme.js
@@ -2,31 +2,38 @@
 
 function darkMode() {
     const btnDm = document.getElementById('darkMode')
-    
+
     const html = document.querySelector('html')
 
-    btnDm.addEventListener('click',() => {
+    btnDm.addEventListener('click', () => {
         html.classList.toggle('dark-mode')
         btnDm.classList.toggle('text-white')
 
         const dmPref = localStorage.getItem('darkmode')
 
-        if(!dmPref) {
+        if (!dmPref) {
             localStorage.setItem('darkmode', true)
-          }
-      
-          if(dmPref) {
-            localStorage.removeItem('darkmode')
-          }
+        }
 
-          if(btnDm.classList.contains('text-white')) {
+        if (dmPref) {
+            localStorage.removeItem('darkmode')
+        }
+
+        if (btnDm.classList.contains('text-white')) {
             btnDm.innerText = 'brightness_7'
-           
-          } else {
+
+        } else {
             btnDm.innerText = "clear_night"
-          }
+        }
     })
 }
+
+const dmPref = localStorage.getItem('darkmode')
+
+  if(dmPref) {
+    document.querySelector('html').classList.add('dark-mode')
+  }
+
 
 darkMode()
 
